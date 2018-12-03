a868c05a4414c2e53cd989e84618d3088fd50405
Adam Kapica <adam.kapica@gmail.com>
2018-12-03 20:13:54 +0100

    Created graph class

---
+++ Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adam on 03.12.18.
+//
+
+#include "Graph.h"
---
+++ Graph.h
@@ -0,0 +1,16 @@
+//
+// Created by adam on 03.12.18.
+//
+
+#ifndef AAL_GRAPH_H
+#define AAL_GRAPH_H
+
+
+
+class Graph {
+
+};
+
+
+
+#endif //AAL_GRAPH_H
