3743a5023edf83c07931f390a384d7b025d42f58
Adam Kapica <adam.kapica@gmail.com>
2018-12-03 20:14:04 +0100

    Created graph class

--- Graph.h
+++ Graph.h
@@ -1,16 +1,23 @@
 //
-// Created by adam on 03.12.18.
+// Created by Adam on 03.12.18.
 //
 
 #ifndef AAL_GRAPH_H
 #define AAL_GRAPH_H
 
-
+#include <vector>
 
 class Graph {
 
-};
+    unsigned vertex;
+    unsigned edge;
+    std::vector<std::vector<int> > edgesList;
 
+public:
+    Graph();
+    Graph(unsigned vertex, unsigned edge, std::vector<std::vector<int> > edgesList);
+    void addEdge(unsigned v1, unsigned v2);
+};
 
 
 #endif //AAL_GRAPH_H
